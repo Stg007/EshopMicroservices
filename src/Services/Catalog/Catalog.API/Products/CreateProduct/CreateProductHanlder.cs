2a24d15c7a330efcdcca08eb69eab1f4efbd7b97
elma taha <azerty0017@live.fr>
2025-12-17 14:54:54 +0100

    First step

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHanlder.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHanlder 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product would go here.
+        
+
+        var product = new Product()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // save to database
+
+
+        // return createProductResult result
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
