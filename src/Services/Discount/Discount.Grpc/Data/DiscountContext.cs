5f79f4127632bcddf6eb4e1308029bb9f886dada
elma taha <azerty0017@live.fr>
2026-01-13 12:10:25 +0100

    Commit work 13 Jan 2026

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Models.Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options):base(options)
+    {
+
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Models.Coupon>().HasData(
+            new Models.Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 10 } ,
+            new Models.Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 12 }
+            );
+    }
+
+}
